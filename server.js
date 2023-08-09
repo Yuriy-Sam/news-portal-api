320f17ac31fbb41caaf7c1fd5d38fa668070a9dc
Yuriy Samoilenko <yuriy.samoilenkoff@gmail.com>
2023-08-09 18:42:23 +0200

    first commit2

--- server.js
+++ server.js
@@ -15,12 +15,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/upload_files", upload.single("avatar"), uploadFiles);
+app.post("/upload_files", upload.single("avatar"), (req, res, next) => {
+  console.log(req.file);
+  return res.json({
+    status: 200,
+    file: req.file.filename,
+    message: "Successfully uploaded files",
+  });
+});
+// app.post("/upload_files", upload.single("avatar"), uploadFiles);
 
 function uploadFiles(req, res) {
-  console.log(req.body);
-  console.log(req.file);
-  res.json({ message: "Successfully uploaded files" });
+  //   console.log(req.file);
+  return res.json({
+    status: 200,
+    file: req.file.filename,
+    message: "Successfully uploaded files",
+  });
 }
 app.listen(5000, () => {
   console.log(`Server started...`);
